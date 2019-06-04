52a5bae2e28c99f0d2613bba340c45413af83fab
Chris Rendle-Short <chris@killred.net>
2019-06-05 08:40:02 +1000

    Update comments.

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,14 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# See configuration.nix(5) man page and/or `nixos-help`.
 
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  # UEFI me
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -227,10 +225,7 @@
     extraGroups = [ "wheel" "networkmanager" "camera" ];
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
+  # Do not change.
   system.stateVersion = "19.03"; # Did you read the comment?
 
 }
