2bf1c0fffd40f908e0fa8f377fcb52ab12d02f78
Chris Rendle-Short <chris@killred.net>
2019-08-20 10:27:49 +1000

    Re-enable CPU governor settings. When using only HWP governor would return to `performance` after resume on battery.

--- configuration.nix
+++ configuration.nix
@@ -245,6 +245,8 @@
   services.tlp = {
     enable = true;
     extraConfig = ''
+      CPU_SCALING_GOVERNOR_ON_AC=performance
+      CPU_SCALING_GOVERNOR_ON_BAT=powersave
       CPU_HWP_ON_AC=performance
       CPU_HWP_ON_BAT=balance_performance
     '';
